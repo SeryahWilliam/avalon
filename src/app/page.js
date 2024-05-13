76101299ab39d8b792392fd52805b0421ae8d02d
SeryahWilliam <161340384+SeryahWilliam@users.noreply.github.com>
2024-05-13 15:48:46 -0500

    created multiple cards

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
     { title: "Clothing", image: "/images/clothes1.jpeg" },
     { title: "Beauty", image: "/images/beauty.jpeg" },
     { title: "Appliances", image: "/images/appliances2.jpeg" },
+    { title: "Gifts for Him", image: "/images/him.webp" },
+    { title: "Gifts for Her", image: "/images/her.jpeg" },
+    { title: "Wedding Gifts", image: "/images/wedding.jpg" },
   ];
   return (
     <div>
@@ -13,7 +16,7 @@ export default function Home() {
         <h4>Stuff</h4>
       </div>
       <h2>Shop Popular Categories</h2>
-      <div className="flex flex-row">
+      <div className="flex flex-row flex-wrap">
         {categories.map((category) => (
           <CategoryCard title={category.title} img={category.image} />
         ))}
