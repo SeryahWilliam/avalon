4219e91a2ea8a64a59e3e24e268ec7ab32766216
SeryahWilliam <161340384+SeryahWilliam@users.noreply.github.com>
2024-05-13 15:12:09 -0500

    made cards for popular category items

--- src/app/components/CategoryCard.jsx
+++ src/app/components/CategoryCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function CategoryCard({ img, title }) {
   return (
-    <div className="flex flex-col items-center h-[23rem] w-[18rem] border-solid border-2 border-grey rounded-lg hover:shadow-lg">
+    <div className="flex flex-col items-center h-[20rem] w-[15rem] m-4 border-solid border-2 border-grey rounded-lg hover:shadow-lg">
       <div className="w-full h-3/4 mb-10">
         <Image
           src={img}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
 import CategoryCard from "./components/CategoryCard";
 
 export default function Home() {
-  const categories = [{ title: "Clothing", image: "/images/clothes1.jpeg" }];
+  const categories = [
+    { title: "Clothing", image: "/images/clothes1.jpeg" },
+    { title: "Beauty", image: "/images/beauty.jpeg" },
+    { title: "Appliances", image: "/images/appliances2.jpeg" },
+  ];
   return (
     <div>
       <h1>Avalon</h1>
-      {categories.map((category) => (
-        <CategoryCard title={category.title} img={category.image} />
-      ))}
+      <div>
+        <h4>Stuff</h4>
+      </div>
+      <h2>Shop Popular Categories</h2>
+      <div className="flex flex-row">
+        {categories.map((category) => (
+          <CategoryCard title={category.title} img={category.image} />
+        ))}
+      </div>
     </div>
   );
 }
