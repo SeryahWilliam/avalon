6943e5131683e1ee6cca43f95decdbddb64d0476
SeryahWilliam <161340384+SeryahWilliam@users.noreply.github.com>
2024-05-13 14:40:52 -0500

    styles card component

---
+++ src/app/components/CategoryCard.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+function CategoryCard({ img, title }) {
+  return (
+    <div className="flex flex-col items-center h-[23rem] w-[18rem] border-solid border-2 border-grey rounded-lg hover:shadow-lg">
+      <div className="w-full h-3/4 mb-10">
+        <Image
+          src={img}
+          width={800}
+          height={600}
+          objectFit="cover"
+          alt=""
+          className="rounded-lg"
+        />
+      </div>
+      <div>{title}</div>
+    </div>
+  );
+}
+
+export default CategoryCard;
