636a8f3e6b2dd4603687b7b0b4238f0712923bcd
SeryahWilliam <161340384+SeryahWilliam@users.noreply.github.com>
2024-05-21 16:06:10 -0500

    edit item image

--- src/app/items/[itemId]/page.jsx
+++ src/app/items/[itemId]/page.jsx
@@ -5,8 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "@/app/redux/slices/cartSlice";
 import { saveCart } from "@/app/redux/actions/cartThunks";
 import { useSession } from "next-auth/react";
-import { Card, Button } from "flowbite-react";
+import { Button, Carousel } from "flowbite-react";
 import Loader from "@/app/components/Loader";
+import CarouselComponent from "@/app/components/Carousel";
 
 function ItemPage() {
   const router = useRouter();
@@ -66,30 +67,44 @@ function ItemPage() {
 
   return (
     <div className="container mx-auto p-6">
-      <Card imgAlt={item.name} imgSrc={item.displayImage}>
-        <h5 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {item.name}
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {item.description}
-        </p>
-        <div className="flex items-center mt-2.5 mb-5">
-          <span className="bg-cyan-100 text-cyan-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-cyan-200 dark:text-cyan-800">
-            {item.rating} / 5
-          </span>
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-1/2">
+          <Carousel className="h-80 md:h-[60vh]">
+            {[item.displayImage, ...item.images].map((image, idx) => (
+              <img
+                src={image}
+                key={idx}
+                alt={`carousel-image-${idx}`}
+                className="object-cover w-[30vw] h-full"
+              />
+            ))}
+          </Carousel>
         </div>
-        <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            ${item.price}
-          </span>
-          <Button
-            onClick={handleAddToCart}
-            className="bg-cyan-700 text-white hover:bg-cyan-800"
-          >
-            Add to cart
-          </Button>
+        <div className="md:w-1/2 md:pl-8">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
+            {item.name}
+          </h1>
+          <p className="mt-4 text-gray-700 dark:text-gray-400">
+            {item.description}
+          </p>
+          <div className="flex items-center mt-4 mb-6">
+            <span className="bg-cyan-100 text-cyan-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-cyan-200 dark:text-cyan-800">
+              {item.rating ?? "0.0"}
+            </span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              ${item.price}
+            </span>
+            <Button
+              onClick={handleAddToCart}
+              className="bg-cyan-700 text-white hover:bg-cyan-800"
+            >
+              Add to cart
+            </Button>
+          </div>
         </div>
-      </Card>
+      </div>
     </div>
   );
 }
