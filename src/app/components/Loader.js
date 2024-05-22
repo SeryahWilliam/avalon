11d861fd03f03373dd83479aa56a7adfbc66fcb3
SeryahWilliam <161340384+SeryahWilliam@users.noreply.github.com>
2024-05-22 08:16:54 -0500

    increase loader page height

--- src/app/components/Loader.js
+++ src/app/components/Loader.js
@@ -3,11 +3,13 @@ import { Spinner } from "flowbite-react";
 
 function Loader() {
   return (
-    <Spinner
-      color="warning"
-      className="w-full h-40"
-      aria-label="Loading spinner"
-    />
+    <div className="flex items-center justify-center h-[66vh]">
+      <Spinner
+        color="warning"
+        className="w-full h-40"
+        aria-label="Loading spinner"
+      />
+    </div>
   );
 }
 
